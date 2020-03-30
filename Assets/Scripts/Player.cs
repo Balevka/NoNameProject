e61b999138aa5fa1e9bba570d37088ed75d7735a
Ivan Mezh <vfeodorovm@yandex.com>
2020-03-30 04:09:42 +0300

    [Player] Added movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 2;
+
+    private Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed);
+        
+        
+    }
+}
