937d5402f3825ebb6fa6f989f86953b1a0d2129b
Ivan Mezh <vfeodorovm@yandex.com>
2020-03-31 04:07:44 +0300

    Create a CameraFollow class for camera follows game object and now Camera follows the Player!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 2;
+    public Camera cam;
 
     private Rigidbody2D rb;
+    private CameraFollow Follow;
+
+
+    
+    
     
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();    
+        rb = GetComponent<Rigidbody2D>();
+        Follow = new CameraFollow(cam, gameObject);
+        
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
-
         rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed);
-        
+        Follow.Follow();
         
     }
 }
