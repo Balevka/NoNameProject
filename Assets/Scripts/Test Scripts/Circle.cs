e3dbf431b5bd2d5ea530bdfcfbebc43c7df29281
Ivan Mezh <vfeodorovm@yandex.com>
2020-04-14 03:42:59 +0300

    Добавлена папка с тестовыми скриптами и скриптами в них

---
+++ Assets/Scripts/Test Scripts/Circle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    public void TeleportUp()
+    {
+        transform.Translate(Vector2.up);
+    }
+}
+
+
+
---
+++ Assets/Scripts/Test Scripts/Health.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public delegate void StateChange(int state);
+    public static event StateChange OnStateChanged;
+
+    public int state = 100;
+
+    private void Update()
+    {
+        OnStateChanged.Invoke(state--);
+    }
+
+}
